5baa738a029b9589b4c5b734262c7044fb368825
benjaminllamas1 <119452190+benjaminllamas1@users.noreply.github.com>
2023-02-09 19:47:29 -0700

    Add files via upload

---
+++ cards/DeckDriver.java
@@ -0,0 +1,16 @@
+package com.example.cards;
+
+public class DeckDriver {
+    public static void main(String[] args) {
+
+        Deck cards = new StandardDeck();
+        displayDeck(cards);
+
+        cards.shuffle();
+        displayDeck(cards);
+    }
+
+    private static void displayDeck(Deck cards) {
+        System.out.println(cards);
+    }
+}
